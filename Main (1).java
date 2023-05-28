cd3500d8cd15c0f4c468514c172e951b156fda3d
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-05-27 22:40:11 -0400

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -14,3 +14,10 @@ class Main {
 }
 }
 
+//Write a program to complete the task given below:
+
+//Ask the user to enter any 2 numbers in between 1-10 and add both of
+//them to another variable called z.
+//Use z for adding 30 into it and print the final result by using variable
+//results.
+
