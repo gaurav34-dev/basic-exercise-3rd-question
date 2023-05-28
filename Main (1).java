6c8f988213a16320b8c06c9a9721f266e15bc100
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-05-27 22:33:09 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("please enter two numbers");
+    Scanner s= new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    //b=(a*b)/b;
+    //a=(a*b)/a;
+    //System.out.println("value of a is "+ a);
+    //System.out.println("value of b is "+ b);
+    int z=a+b;
+    System.out.println(z+30);
+}
+}
+
+
+//swap two numbers without third variable
